75adb6050eb1fca09a2690eef5ac31d5e8f907c3
Javier Godoy Núñez <javier@oxozon.io>
2025-03-21 11:46:01 -0400

    fix: Login background should cover entire screen

--- app/pages/auth/login.tsx
+++ app/pages/auth/login.tsx
@@ -8,7 +8,7 @@ import { LoginForm } from "~/components/login-form"
  */
 export default function LoginPage() {
   return (
-    <div style={{ background: 'url(/login.background.png)' }} className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <div style={{ background: 'url(/login.background.png)', backgroundSize: 'cover' }} className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
